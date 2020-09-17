60c372afb16c52bdab02fe12c9e1ea8517d4f5f5
Tabatha Macedo <33585625+tabskey@users.noreply.github.com>
2020-09-17 20:49:32 -0300

    Created the project with temporary axios solution

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/sheep.png" width="80">
+      <hr>
+      <h4 class="is-size-4 has-text-weight-medium">Pokedex</h4>
+      <input class="input is-danger is-rounded" type="text" name="" id=""
+      placeholder="Catch a new pokemon by their name!" v-model="search">
+      <button class="button is-primary is-light" id="searchBtn" @click="searchBy">Catch'em!</button>
+      <div v-for="(poke,index) in filteredPoke" :key="poke.url"> <!-- or searchResults -->
+       <Pokenent :name="poke.name" :url="poke.url" :num="index + 1"/>
+      <!--<h3> {{ index ++}} {{poke.name}}</h3> -->
+      </div>
+     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import axios from "axios";
+  import Pokenent from './components/Pokenent'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      pokemons: [],
+      search:'',
+      filteredPoke: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then( res => {
+     // console.log(res.data.results);
+     this.pokemons = res.data.results;
+     this.filteredPoke = res.data.results;
+     console.log(this.pokemons);
+    })
+  },
+  components:{
+    Pokenent
+  },
+  methods:{
+    searchBy: function(){
+      var pokemonMin = this.search.toLowerCase();
+      this.filteredPoke = this.pokemons;
+      if(this.search =='' || this.search == ' ') {
+        this.filteredPoke = this.pokemons;
+    }else{
+      this.filteredPoke = this.pokemons.filter(pokemon => pokemon.name == pokemonMin)
+    }
   }
+  // computed:{
+  //   searchResults: function() {
+  //     var pokemonMin = this.search.toLowerCase();
+  //     if(pokemonMin =='' || pokemonMin == ' ') {
+  //       return this.pokemons;
+  //     }else{
+  //       return this.pokemons.filter(pokemon => pokemon.name == pokemonMin);
+  //     }
+  //   }
+  }
+ 
 }
 </script>
 
@@ -24,5 +70,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#searchBtn {
+  margin-top: 2%;
 }
 </style>
---
+++ src/components/Pokenent.vue
@@ -0,0 +1,90 @@
+<template>
+<div id="pokemon">
+ <div class="card">
+  <div class="card-image">
+    <figure>
+      <img :src="currentSprite" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">{{num}} {{name | upperCase}}</p>
+        <div class="tags column is-half is-offset-one-quarter">
+            <span class="tag">{{ pokemon.types | upperCase }}</span>
+            <span class="tag" v-if="pokemon.secType">{{ pokemon.secType | upperCase}} </span>
+        </div>  
+      </div>
+
+    </div>
+
+    <div class="content">
+        <button class="button is-danger is-rounded" @click="changeSprite">Change Sprite</button>
+      
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    created: function(){
+        axios.get(this.url).then(res => {
+            if(res.data.types.length > 1){
+                this.pokemon.types = res.data.types;
+            }
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            this.currentSprite =  this.pokemon.front;
+            
+         }) ,
+            axios.get(this.url).then(res => {
+            this.pokemon.secType = res.data.types[1].type.name;
+         }).catch(function() {
+            this.pokemon.secType = false; 
+         })
+    },
+    data(){
+      return {
+          isFront: true,
+          currentSprite: '',
+          pokemon:{
+              type: '',
+              secType: '',
+              front: '',
+              back: ''
+          }
+      }  
+    },
+    props:{
+        num: Number,
+        name : String,
+        url : String
+    },
+    filters:{
+        upperCase: function(value) {
+            var nameUpper = value[0].toUpperCase() + value.slice(1); 
+            return nameUpper;
+        }
+    },
+    methods: {
+        changeSprite: function() {
+            if(this.isFront) {
+                this.isFront = false;
+                this.currentSprite = this.pokemon.back
+            }else {
+                this.isFront = true;
+                this.currentSprite = this.pokemon.front;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+#pokemon {
+    margin-top: 2%;
+}
+</style>
